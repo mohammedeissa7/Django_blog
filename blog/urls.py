5aa82f9e29768ec36982958d1af10865915bfe21
Eissa <smme012345@gmail.com>
2025-03-11 04:43:17 +0200

    #3 Create_Update_Delete_Detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostUpdateView, PostDetailView, PostDeleteView, PostCreateView
 
 
 
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('detail/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('update/<int:pk>', PostUpdateView.as_view(), name='post-update'),
+    path('delete/<int:pk>', PostDeleteView.as_view(), name='post-delete'),
+    path('new/', PostCreateView.as_view(), name='post-create'),  
     path('about/', views.about, name='blog-about'),   
 ]
